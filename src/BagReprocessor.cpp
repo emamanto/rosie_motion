6d9015d3fd0bceb97c72e74655e8fd946c8bb531
Lizzie Mamantov <mamantov@umich.edu>
2019-04-02 16:43:48 -0400

    Loads all the other messages, matches info that corresponds to each traj.

--- src/BagReprocessor.cpp
+++ src/BagReprocessor.cpp
@@ -9,8 +9,6 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Float32.h"
 
-#include "ObjectDatabase.h"
-#include "WorldObjects.h"
 #include "ArmController.h"
 
 class BagReprocessor {
@@ -70,6 +68,80 @@ public:
         }
 
         arm.updateCollisionScene(s->collision_objects);
+
+
+        std::vector<std::string> dataTopics;
+        dataTopics.push_back(std::string("planners"));
+        dataTopics.push_back(std::string("targets"));
+        dataTopics.push_back(std::string("planning_times"));
+        dataTopics.push_back(std::string("start_states"));
+        dataTopics.push_back(std::string("trajectories"));
+
+        rosbag::View dataView(bagFile, rosbag::TopicQuery(dataTopics));
+
+        std_msgs::String::ConstPtr pl;
+        std_msgs::Float32::ConstPtr pt;
+        sensor_msgs::JointState::ConstPtr ss;
+        geometry_msgs::Transform::ConstPtr xf;
+        trajectory_msgs::JointTrajectory::ConstPtr traj;
+
+        // pl, pt, ss, xf, traj
+        std::vector<ros::Time> msgTimes;
+        msgTimes.resize(5);
+
+        int numTrajectories = 0;
+
+        for (rosbag::MessageInstance const m: dataView)
+        {
+            bool allMatched = true;
+            for (int i = 1; i < msgTimes.size(); i++) {
+                if (msgTimes[i] != msgTimes[i-1]) {
+                    allMatched = false;
+                    break;
+                }
+            }
+            // Where replaying the trajectory can happen!
+            if (allMatched) {
+                numTrajectories++;
+            }
+
+            std_msgs::String::ConstPtr tmpPl = m.instantiate<std_msgs::String>();
+            if (tmpPl != NULL) {
+                pl = tmpPl;
+                msgTimes[0] = m.getTime();
+                continue;
+            }
+
+            std_msgs::Float32::ConstPtr tmpPt = m.instantiate<std_msgs::Float32>();
+            if (tmpPt != NULL) {
+                pt = tmpPt;
+                msgTimes[1] = m.getTime();
+                continue;
+            }
+
+            sensor_msgs::JointState::ConstPtr tmpSs = m.instantiate<sensor_msgs::JointState>();
+            if (tmpSs != NULL) {
+                ss = tmpSs;
+                msgTimes[2] = m.getTime();
+                continue;
+            }
+
+            geometry_msgs::Transform::ConstPtr tmpXf = m.instantiate<geometry_msgs::Transform>();
+            if (tmpXf != NULL) {
+                xf = tmpXf;
+                msgTimes[3] = m.getTime();
+                continue;
+            }
+
+            trajectory_msgs::JointTrajectory::ConstPtr tmpTraj = m.instantiate<trajectory_msgs::JointTrajectory>();
+            if (tmpTraj != NULL) {
+                traj = tmpTraj;
+                msgTimes[4] = m.getTime();
+                continue;
+            }
+        }
+
+        ROS_INFO("Found %i trajectories in the .bag file!", numTrajectories);
         return true;
     }
 
@@ -80,8 +152,6 @@ public:
 private:
     ros::NodeHandle n;
     rosbag::Bag bagFile;
-    //WorldObjects world;
-    //ObjectDatabase objData;
     ArmController arm;
 
     bool ready;
